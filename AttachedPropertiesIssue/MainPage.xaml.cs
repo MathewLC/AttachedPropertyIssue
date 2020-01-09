e3435a2404f45f3c36c66beaa13bc62af9e18137
MatthewLC <mateusluiztj@hotmail.com>
2020-01-09 11:58:33 -0300

    First Commit

---
+++ AttachedPropertiesIssue/MainPage.xaml.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace AttachedPropertiesIssue
+{
+    // Learn more about making custom code visible in the Xamarin.Forms previewer
+    // by visiting https://aka.ms/xamarinforms-previewer
+    [DesignTimeVisible(false)]
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private async void Button_Clicked(object sender, EventArgs e)
+        {
+            var properties1 = StackLayoutProperties.GetStackLayoutCollection(StackLayout1); //this should return only StackLayout1 property
+            var properties2 = StackLayoutProperties.GetStackLayoutCollection(StackLayout2); //this should return only StackLayout2 property
+
+            var str = "";
+            int count = 0;
+            foreach (var prop in properties1)
+            {
+                str += $"{count} properties1: {prop.ExampleProperty1} \n";
+                count++;
+            }
+
+            foreach (var prop in properties2)
+            {
+                str += $"{count} properties2: {prop.ExampleProperty1} \n";
+                count++;
+            }
+
+            await DisplayAlert("Title", str, "OK");
+
+        }
+    }
+}
---
+++ AttachedPropertiesIssue/StackLayoutProperties.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xamarin.Forms;
+
+namespace AttachedPropertiesIssue
+{
+
+    public class MyCustomClass
+    {
+        public string ExampleProperty1 { get; set; }
+    }
+
+    public class StackLayoutProperties
+    {
+        public static readonly BindableProperty StackLayoutCollectionProperty =
+            BindableProperty.CreateAttached("StackLayoutCollection",
+                typeof(IList<MyCustomClass>),
+                typeof(StackLayoutProperties),
+                new List<MyCustomClass>());
+
+        public static IList<MyCustomClass> GetStackLayoutCollection(BindableObject view) => (IList<MyCustomClass>)view.GetValue(StackLayoutCollectionProperty);
+        public static void SetStackLayoutCollection(BindableObject view, IList<MyCustomClass> value) => view.SetValue(StackLayoutCollectionProperty, value);
+    }
+}
